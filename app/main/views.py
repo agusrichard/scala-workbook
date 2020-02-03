5511e48586b20b0acbf49999f5ff8b724fdf6e17
Agus Richard <agus.richard21@gmail.com>
2020-02-03 01:40:26 +0000

    Posts pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, abort, flash, request
+from flask import render_template, redirect, url_for, abort, flash, request, current_app
 from flask_login import login_required, current_user, login_user
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -31,15 +31,23 @@ def home():
 		db.session.add(post)
 		db.session.commit()
 		return redirect(url_for('main.home'))
-	posts = Post.query.order_by(Post.timestamp.desc()).all()
-	return render_template('main/home.html', form=form, posts=posts, title='Home')
+	page = request.args.get('page', 1, type=int)
+	pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+			page, per_page=current_app.config['TALOS_POSTS_PER_PAGE'],
+			error_out=False)
+	posts = pagination.items
+	return render_template('main/home.html', form=form, posts=posts, pagination=pagination, title='Home')
 
 
 @main.route('/user/<username>')
 def user(username):
 	user = User.query.filter_by(username=username).first_or_404()
-	posts = user.posts.order_by(Post.timestamp.desc()).all()
-	return render_template('main/user.html', user=user, posts=posts, title=user.username)
+	page = request.args.get('page', 1, type=int)
+	pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+			page, per_page=current_app.config['TALOS_POSTS_PER_PAGE'],
+			error_out=False)
+	posts = pagination.items
+	return render_template('main/user.html', user=user, posts=posts, pagination=pagination, title=user.username)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
