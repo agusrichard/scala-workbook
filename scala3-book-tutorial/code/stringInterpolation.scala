965b8022d55e8f30237b4104f7025ade6af555d5
Agus Richard <agus.richard21@gmail.com>
2024-02-14 21:13:01 +0700

    code for string interpolation

---
+++ scala3-book-tutorial/code/stringInterpolation.scala
@@ -0,0 +1,6 @@
+@main def stringInterpolation() =
+    val ara = "Damara Astiningtyas"
+    val richie = "Agus Richard Lubis"
+    val verb = "Loves"
+    val sentence = s"${ara} ${verb} ${richie}"
+    println(sentence)
