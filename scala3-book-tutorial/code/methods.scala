c1ae67e83eea658b302372d64235279e06138079
Agus Richard <agus.richard21@gmail.com>
2024-02-16 20:13:12 +0700

    simple summation with for loop

--- scala3-book-tutorial/code/methods.scala
+++ scala3-book-tutorial/code/methods.scala
@@ -1,5 +1,11 @@
 def multiplyList(nums: List[Int], multiplier: Int): List[Int] =
   nums.map(_ * multiplier)
 
+def simpleSummation(nums: List[Int]): Int =
+  var acc = 0
+  for i <- nums do acc += i
+  acc
+
 @main def methods() =
   println(multiplyList(List(1, 2, 3, 4, 5), 10))
+  println(simpleSummation(List(1,2,3,4,5)))
