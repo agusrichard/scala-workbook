bc15cc5dfef7fd83fc72a120c1c577060376e157
Agus Richard <agus.richard21@gmail.com>
2024-05-15 20:55:39 +0700

    practice 002 basic mathematical operations

---
+++ coding-challenges/prac_002_basic_mathematical_operations/code.scala
@@ -0,0 +1,11 @@
+def basicOp(op: Char, a: Int, b: Int): Int =
+  if op == '+' then
+    a + b
+  else if op == '-' then
+    a - b
+  else if op == '*' then
+    a * b
+  else if op == '/' then
+    a / b
+  else
+    throw new RuntimeException("Please provide the correct operation: +, -, *, /")
