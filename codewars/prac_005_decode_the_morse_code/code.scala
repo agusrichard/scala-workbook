7143c7f6c42e166477a700d5f9915b1a31db4c69
Agus Richard <agus.richard21@gmail.com>
2024-05-26 12:19:05 +0700

    decode the morse code

---
+++ codewars/prac_005_decode_the_morse_code/code.scala
@@ -0,0 +1,15 @@
+object MorseDecoder {
+  import MorseCodes.morseCodes
+
+  def decode(msg: String): String = {
+    val words = msg.split("   ") // split for words
+
+    val decodedWords = for word <- words yield {
+      val chars = word.split(" ") // split for characters
+      val decodedChars = for char <- chars yield morseCodes(char)
+      decodedChars.mkString
+    }
+
+    decodedWords.mkString(" ")
+  }
+}
