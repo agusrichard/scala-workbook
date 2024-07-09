6bcd0c1f1c37a16aaa62d9adfb0a7754d2c85e3d
Agus Richard <agus.richard21@gmail.com>
2024-07-09 20:37:59 +0700

    i'm in vacation

--- spark-with-scala-course/src/main/scala/Main.scala
+++ spark-with-scala-course/src/main/scala/Main.scala
@@ -4,6 +4,6 @@ import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Trust me, I'm learning it now")
+    println("I'm in vacation")
   }
 }
